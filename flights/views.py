1596c1998fdcd58bafb8a3e2640ee804c4c9b793
Ghada6336 <ghadafahmie6336@gmail.com>
2020-02-24 23:13:41 +0200

    finished

--- flights/views.py
+++ flights/views.py
@@ -1,9 +1,9 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView, DestroyAPIView , CreateAPIView
 from datetime import datetime
 
 from .models import Flight, Booking
-from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer
-
+from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer 
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 class FlightsList(ListAPIView):
 	queryset = Flight.objects.all()
@@ -33,3 +33,12 @@ class CancelBooking(DestroyAPIView):
 	queryset = Booking.objects.all()
 	lookup_field = 'id'
 	lookup_url_kwarg = 'booking_id'
+
+
+class BookingCreate(CreateAPIView):
+	serializer_class = UpdateBookingSerializer
+
+	def perform_create(self , serializer):
+		flight_id =self.kwargs.get("flight_id")
+		flight_object= Flight.objects.get(id=flight_id)
+		serializer.save(flight=flight_object , user=self.request.user)
